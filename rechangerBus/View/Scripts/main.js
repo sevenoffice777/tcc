68bf10e4abe836624ce4983af963e4eee629ff2a
unknown <Aluno@LS17M11.etecra.local>
2024-05-08 14:43:01 -0300

    feat: landingPage edited

--- rechangerBus/View/Scripts/main.js
+++ rechangerBus/View/Scripts/main.js
@@ -1,6 +1,29 @@
+let container = document.querySelector(".container")
+let log = document.querySelector(".log");
+let img_loading = document.querySelector("#load_img");
+let button_start = document.querySelector("#btn_iniciar");
 
-        window.onload = () =>{
-            setTimeout(()=>{
-                
-            }, 2000);
-        }
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        toggleClass(img_loading, "display_none");
+        toggleClass(button_start, "display_none");
+    }, 3000)
+
+})
+
+button_start.addEventListener("click", ()=>{
+    toggleClass(log, "display_none");
+    // Desaparece o log de carregamento
+
+    toggleClass(document.querySelector(".nav-contain"), "display_none");
+
+})
+    
+
+function toggleClass(classVerify, classAlter) {
+    if (classVerify.classList.contains(classAlter)) {
+        classVerify.classList.remove(classAlter);
+    } else {
+        classVerify.classList.add(classAlter);
+    }
+}
