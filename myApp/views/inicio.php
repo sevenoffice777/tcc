8600d325ce251f4e31916bc7a6911848576c2535
Samuel Seven <sevenofice777@gmail.com>
2024-06-07 09:30:53 -0300

    feat: começando a construir a pagina inicial do usuario

---
+++ myApp/views/inicio.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rechanger Bus | </title>
+    <!-- Incrementado com o nome do cliente posteriormente com algoritmo de inserção ja implementado em outro projeto -->
+
+    <link rel="stylesheet" href="./styles/general.css">
+    <link rel="stylesheet" href="./styles/inicio.css">
+    <link rel="shortcut icon" href="../assets/favicon.png" type="image/x-icon">
+</head>
+
+<body>
+    <div class="container">
+       
+    </div>
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
