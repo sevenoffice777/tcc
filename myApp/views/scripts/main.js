23ffc0cfb1ba973b3d8ddc073505002093e48baf
sevenoffice777 <sevenofice777@gmail.com>
2024-06-14 08:56:13 -0300

    feat: refatored

--- myApp/views/scripts/main.js
+++
@@ -1,8 +0,0 @@
-function toggleClass(elementVerify, elementAlter) {
-    if (elementVerify.classList.contains(elementAlter)) {
-        elementVerify.classList.remove(elementAlter);
-    } else {
-        elementVerify.classList.add(elementAlter);
-    }
-
-}
