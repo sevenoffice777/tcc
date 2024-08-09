e14ace1b9fce3377b0018ec09b05778910acccd0
Seven <sevenofice777@gmail.com>
2024-08-09 09:34:19 -0300

    feat: refactor all

---
+++ landing_page.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rechanger Bus | Sejam Bem-Vindos</title>
+
+    <!-- Icon -->
+    <link rel="shortcut icon" href="./assets/img/bus.ico" type="image/x-icon">
+
+    <!-- Style -->
+    <link rel="stylesheet" href="./view/styles/style.css">
+
+    <!-- Imports -->
+</head>
+
+<body>
+    <header>
+        <img src="./assets/img/logo.png" alt="Logo Rechanger Bus" id="logo-header">
+        <nav>
+            <a href="">ENTRAR</a>
+            <a href="">CADASTRAR</a>
+        </nav>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="left">
+                <div class="card">
+                    <h1>Rechanger Bus</h1>
+                    <p>Agora a facilidade está em suas mãos!</p>
+                    <button>Junte-se conosco</button>
+                </div>
+            </div>
+            <div class="right">
+                <img src="./assets/img/building.png" alt="Building-bottom" id="building">
+            </div>
+        </div>
+    </main>
+
+
+
+    <!-- -->
+
+
+</body>
+
+</html>
