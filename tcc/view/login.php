48cf8869686ef686846344e8d1a73e7e8ee20b83
Samuel Seven <sevenofice777@gmail.com>
2024-09-03 09:12:43 -0300

    feat: login.php

---
+++ tcc/view/login.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Rechanger Bus | Login </title>
+
+    <link rel="shortcut icon" href="../assets/logo.png" type="image/x-icon">
+
+    <link rel="stylesheet" href="../styles/reset.css">
+    <link rel="stylesheet" href="../styles/login.css">
+
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="left">
+            <div class="login-card">
+                <h1>LOGIN</h1>
+                <label for="cpf_inpt">CPF</label>
+                <input type="number" name="cpf" id="cpf-inpt" class="inputs-prompt">
+                <label for="password_inpt">SENHA</label>
+                <input type="password" name="password" id="password-inpt" class="inputs-prompt">
+                <span id="lostPassword">ESQUECEU A SENHA?</span>
+                <input type="submit" id="sbmit-btn">
+                <a href="./view/signup.php" id="toCreateAccount">NÃO TEM CONTA?</a>
+            </div>
+        </div>
+        <div class="right-none">
+            <div class="text">
+                <span id="frase1-impacto">
+                    Pronto para o proximo destino?
+                </span>
+                <span id="frase2-impacto">
+                    O poder de se mover está em suas mãos
+                </span>
+            </div>
+            <img src="../assets/onibus-img-canto.png" id="onibus-canto" alt="Imagem do Onibus no Canto do Site">
+        </div>
+    </div>
+
+    <footer>
+        <div class="section-footer">
+            <img src="../assets/logo.png" alt="LOGO">
+            <button>Fale conosco <ion-icon name="mail-outline"></ion-icon></button>
+            
+        </div>
+        <div class="section-footer">
+            <h2>SEÇÕES</h2>
+            <a href="">Quem Somos</a>
+            <a href="">Nossos Serviços</a>
+            <a href="">Contato</a>
+            <a href="">Parcerias</a>
+        </div>
+        <div class="section-footer">
+            <h2>SUPORTE</h2>
+            <span class="info">Email:  rechangerbus@gmail.com</span>
+            <span class="info">Telefone:  +55 14 91000-9923</span>
+            <span class="info">CNPJ:  123141235124</span>
+        </div>
+        <div class="section-footer">
+            <h2>RECHANGER BUS</h2>
+            <hr>
+            <span>"Facilitando seu dia-a-dia"</span>
+
+        </div>
+    </footer>
+</body>
+
+</html>
